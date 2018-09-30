aa7563173bbaf4268019fdada1238d4f56d46753
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-09-30 17:08:56 -0400

    made hacky sql injection on login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     # logging user information for debugging purposes...
     logger.info(User.find_by_email(params[:email]).as_json)
-    if user && user.authenticate(params[:password])
+    # mimicking sql inj below
+    if user && (user.authenticate(params[:password]) || params[:password].delete(' ').end_with?("\"OR\"1==1") || params[:password].delete(' ').end_with?("\"OR\"0==0") || params[:password].delete(' ').end_with?("\"OR\"2==2") || params[:password].delete(' ').end_with?("\"OR\"3==3") || params[:password].delete(' ').end_with?("\"OR\"TRUE"))
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     elsif user
