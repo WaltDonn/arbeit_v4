d81740f1f9bb34a432e2c5fb46aecbde2079e5ac
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-09-30 20:50:54 -0400

    alex traversal vuln fix

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,8 +1,14 @@
+require 'pathname'
 class StaticsController < ApplicationController
   def show
     @page = params[:page]
      begin
-      render :file => File.join(Rails.root, 'includes', @page)
+      path = Pathname.new(@page).realpath
+      if(path.to_s.start_with?(Rails.root.to_s))
+      	render file: Rails.root.join(path)
+      else
+      	render :action => 'not_found'
+      end
     rescue
       render :action => 'not_found'
     end
